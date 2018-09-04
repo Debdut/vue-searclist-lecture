54b06832b1b702d01174bc53adb23e0870a93126
Debdut Karmakar <iawaiponly@gmail.com>
2018-09-04 12:18:15 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  #app
+    button(@click="updateWords()") Change Data
+    search-list(:data="words")
+</template>
+
+<script>
+import SearchList from "./components/SearchList";
+
+export default {
+  name: "App",
+  components: {
+    SearchList
+  },
+  data() {
+    return {
+      lapse: 2000,
+      words: [
+        "app",
+        "apple",
+        "apple juice",
+        "appy",
+        "mango",
+        "man",
+        "many",
+        "any"
+      ]
+    };
+  },
+  methods: {
+    updateWords() {
+      this.words.shift();
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SearchList.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  .search-list
+    input(v-model="search")
+    ul.list
+      li.list-item(v-for="item in searchResults") {{ item }}
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    data: Array,
+    interval: Number,
+  },
+  data() {
+    return {
+      search: ''
+    }
+  },
+  computed: {
+    searchResults() {
+      return this.data.filter(item => item.indexOf(this.search) === 0)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .list {
+    list-style-type: none;
+    -webkit-padding-start: 20px;
+  }
+</style>
